33872f86dd13e8cd4c30ea3bdc859b179acf0f45
Praise Diratsagae <praisediratsagae@gmail.com>
2019-01-07 12:12:11 +0200

    importing project

---
+++ DifuseBomb/DifuseBomb/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DifuseBomb
+{
+    public partial class MainPage : ContentPage
+    {
+        static string bomb = new Random().Next(1, 4).ToString();
+        static int scores = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            //GAME OVER
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Bomb Exploded","GAME OVER","Retry");
+                bomb = new Random().Next(1, 4).ToString();
+                scores = 0;
+            }
+            else
+            {
+                scores += 1;
+                await DisplayAlert("Bomb Defused", "Scores:" + scores, "Continue");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+        }
+    }
+}
